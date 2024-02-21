01eba7b52d3fade119ca06e77231e83f71bd7253
Choi <june6297@gmail.com>
2024-02-21 16:55:03 +0900

    dd

--- Practice_1/blog_abtest.py
+++ Practice_1/blog_abtest.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template, make_response
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_cors import CORS
 from blog_view import blog
+from blog_control.user_mgmt import User
 import os
 
 # https 만을 지원하는 기능을 http 에서 테스트할 때 필요한 설정
@@ -9,13 +10,21 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
-app.secret_key = 'dave_server'
+app.secret_key = 'choi_server'
 
 app.register_blueprint(blog.blog_abtest, url_prefix='/blog')
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.session_protection = 'strong'
 
+
+@app.route("/login")
+def login():
+    return render_template("login_1.html")
+
+@app.route("/signup")
+def signup(): 
+    return render_template("signup_1.html")
 
 @login_manager.user_loader
 def load_user(user_id):
--- Practice_1/blog_view/blog.py
+++ Practice_1/blog_view/blog.py
@@ -1,7 +1,33 @@
-from flask import Flask, Blueprint, request, render_template
+from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for
+from flask_login import login_user, current_user
+from blog_control.user_mgmt import User
 
 blog_abtest = Blueprint('blog',__name__)
 
-@blog_abtest.route('/test')
-def test():
-    return render_template('blog_A.html')
+@blog_abtest.route('/set_email', methods=['GET','POST'])
+def set_email():
+    if request.method == 'GET':
+        print('set_email', request.args.get('user_email'))
+        return redirect(url_for('blog.test_blog'))
+    else:
+        # print('set_email',request.headers)
+        # content type 이 application/json 인 경우
+        # print('set_email',request.get_json())
+        print('set_email',request.form['user_email'])
+        user = User.create(request.form['user_email'], 'A')
+        login_user(user)
+
+        return redirect(url_for('blog.test_blog'))
+    
+    # print(request.args.get('user_email'))
+    # return redirect(url_for('blog.test_blog'))
+    # return redirect('/blog/test_blog')
+    # return make_response(jsonify(success=True), 200)
+
+
+@blog_abtest.route('/test_blog')
+def test_blog():
+    if current_user.is_authenticated:
+        return render_template('blog_A.html', user_email = current_user.user_email)
+    else:
+        return render_template('blog_A.html')
